756ebcc392496c2fdd21f945d2b4e5391f68f00d
Adam-stac <adammohamed_1@outlook.com>
2026-04-23 16:23:35 +0100

    refactor: simplify AppConfig, remove redundant getClasses method

--- src/main/java/com/smartcampus/AppConfig.java
+++ src/main/java/com/smartcampus/AppConfig.java
@@ -1,22 +1,6 @@
 package com.smartcampus;
-
-import com.smartcampus.resource.DiscoveryResource;
-import com.smartcampus.resource.RoomResource;
-import com.smartcampus.resource.SensorResource;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
 
 @ApplicationPath("/api/v1")
-public class AppConfig extends Application {
-
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> classes = new HashSet<>();
-        classes.add(DiscoveryResource.class);
-        classes.add(RoomResource.class);
-        classes.add(SensorResource.class);
-        return classes;
-    }
-}
+public class AppConfig extends Application { }
